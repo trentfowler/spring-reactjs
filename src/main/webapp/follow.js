40e4d95fa74cca1024a5bfd9f5154887c55c3f15
Trent Fowler <trentfowler@yahoo.com>
2018-02-22 23:49:51 -0600

    function to aid navigation of the REST endpoint

---
+++ src/main/webapp/follow.js
@@ -0,0 +1,47 @@
+/**
+ * Function for navigating the REST endpoint starting from the root to where
+ * you need to go.
+ */
+module.exports = function follow(api, rootPath, relArray) {
+    var self = this;
+    return new Promise(function (self) {
+        var root = api({
+            method: 'GET',
+            path: rootPath
+        });
+
+        return relArray.reduce(function(root, arrayItem) {
+            var rel = typeof arrayItem === 'string' ? arrayItem : arrayItem.rel;
+            return traverseNext(root, rel, arrayItem);
+        }, root);
+
+        function traverseNext (root, rel, arrayItem) {
+            return root.then(function (response) {
+                if (hasEmbeddedRel(response.entity, rel)) {
+                    return response.entity._embedded[rel];
+                }
+
+                if(!response.entity._links) {
+                    return [];
+                }
+
+                if (typeof arrayItem === 'string') {
+                    return api({
+                        method: 'GET',
+                        path: response.entity._links[rel].href
+                    });
+                } else {
+                    return api({
+                        method: 'GET',
+                        path: response.entity._links[rel].href,
+                        params: arrayItem.params
+                    });
+                }
+            });
+        }
+
+        function hasEmbeddedRel (entity, rel) {
+            return entity._embedded && entity._embedded.hasOwnProperty(rel);
+        }
+    });
+}
