bed2a5a51b54932035ef5cf9a0e9a382b8d56ac5
Trent Fowler <trentfowler@yahoo.com>
2018-02-26 00:11:12 -0600

    Overloaded constructor

--- src/main/java/com/trentfowler/springreactjs/template/CustomRectangle.java
+++ src/main/java/com/trentfowler/springreactjs/template/CustomRectangle.java
@@ -22,32 +22,48 @@ public class CustomRectangle {
 
     private @Id @GeneratedValue Long id;
 
-    @Getter @Setter private int x1 = 0;
-    @Getter @Setter private int y1 = 0;
-    @Getter @Setter private int x2 = 0;
-    @Getter @Setter private int y2 = 0;
-
-    /**
-     * Constructor defines a rectangle, must provide bottom left corner
-     * indices (x1, y1) and top right corner indices (x2, y2).
-     *
-     * @param x1 Integer, X-value of the bottom left corner on X-Y coordinate axis.
-     * @param y1 Integer, Y-value of the bottom left corner on X-Y coordindate axis.
-     * @param x2 Integer, X-value of the top right corner on X-Y coordinate axis.
-     * @param y2 Integer, Y-value of the top right corner on X-Y coordinate axis.
-     */
-    public CustomRectangle(int x1, int y1, int x2, int y2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-    }
+    @Getter @Setter private String x1 = "";
+    @Getter @Setter private String y1 = "";
+    @Getter @Setter private String x2 = "";
+    @Getter @Setter private String y2 = "";
 
     /**
      * Default constructor, sets bottom left corner to (0, 0) and sets top
      * right corner to (0,0).
      */
     public CustomRectangle() { }
+
+    /**
+     * Overloaded Constructor, takes bottom left corner indices (x1, y1)
+     * and top right corner indices (x2, y2).
+     *
+     * @param x1 Integer, X-value of the bottom left corner on X-Y coordinate axis.
+     * @param y1 Integer, Y-value of the bottom left corner on X-Y coordinate axis.
+     * @param x2 Integer, X-value of the top right corner on X-Y coordinate axis.
+     * @param y2 Integer, Y-value of the top right corner on X-Y coordinate axis.
+     */
+    public CustomRectangle(int x1, int y1, int x2, int y2) {
+        this.x1 = Integer.toString(x1);
+        this.y1 = Integer.toString(y1);
+        this.x2 = Integer.toString(x2);
+        this.y2 = Integer.toString(y2);
+    }
+
+    /**
+     * Overloaded Constructor, takes bottom left corner indices (x1, y1)
+     * and top right corner indices (x2, y2).
+     *
+     * @param x1 String, X-value of the bottom left corner on X-Y coordinate axis.
+     * @param y1 String, Y-value of the bottom left corner on X-Y coordinate axis.
+     * @param x2 String, X-value of the top right corner on X-Y coordinate axis.
+     * @param y2 String, Y-value of the top right corner on X-Y coordinate axis.
+     */
+    public CustomRectangle(String x1, String y1, String x2, String y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
 
     /**
      * String representation of rectangle object.
